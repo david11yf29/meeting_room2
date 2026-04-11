77fd7970516b43c33bd1b36e8163b8da1e016eea
david11yf29 <david11yf29@gmail.com>
2026-04-11 10:21:09 +0800

    20260411-3

--- include/student.h
+++ include/student.h
@@ -1,6 +1,21 @@
 #pragma once
-#include <iostream>
 #include <string>
 #include "Identity.h"
 
-class Student {};
+class Student : public Identity {
+   public:
+    Student();
+    Student(int id, const std::string& name, const std::string& pwd);
+
+    void openMenu() override;
+
+    void applyOrder();
+    void showMyOrder();
+    void showAllOrder();
+    void cancelOrder();
+
+    int getId() const;
+
+   private:
+    int m_Id;
+};
--- src/student.cpp
+++ src/student.cpp
@@ -1,1 +1,16 @@
-class Student {};
+#include "Student.h"
+
+Student::Student() {}
+Student::Student(int id, const std::string& name, const std::string& pwd) {}
+
+void Student::openMenu() {}
+
+void Student::applyOrder() {}
+
+void Student::showMyOrder() {}
+
+void Student::showAllOrder() {}
+
+void Student::cancelOrder() {}
+
+int Student::getId() const { return 0; }
